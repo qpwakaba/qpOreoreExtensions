9cd67e9209112f4c3579380d5507e55617311c56
King(qpwakaba) <King.qpwakaba@gmail.com>
2019-10-03 17:09:35 +0900

    Fix As method doesn't working

--- CastExtension.cs
+++ CastExtension.cs
@@ -7,16 +7,7 @@ namespace qpwakaba.Extensions
 {
     public static class CastExtension
     {
-        private class DynamicCastCode<T>
-        {
-            private static class Cache<TResult>
-            {
-                public static readonly Func<T, TResult> Cast
-                    = Expression.Lambda<Func<T, TResult>>(Expression.Convert(Expression.Parameter(typeof(T)), typeof(TResult)), Expression.Parameter(typeof(T))).Compile();
-            }
-            public static TResult Cast<TResult>(T value) => Cache<TResult>.Cast(value);
-        }
-        public static TResult As<T, TResult>(this T value)
-            => DynamicCastCode<T>.Cast<TResult>(value);
+        public static TResult As<TResult>(this object value)
+            => (TResult)value;
     }
 }
